f78a6cac3a0316246f41d77b626f7697ca5a374a
Alisher Eshmurodov <eshmurodovalisher@inbox.ru>
2023-09-14 11:26:37 +0500

    made right

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -175,9 +175,6 @@ class PostController extends Controller
         $post->delete();
         return redirect()->route('posts.index');
 
-        //committed
-        //2-commit
-        //3-comit
     }
 }
 
