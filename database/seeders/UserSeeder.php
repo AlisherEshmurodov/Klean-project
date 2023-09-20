732c57dc5a4f0600cf3b5f41b9eadef45af912ca
Alisher Eshmurodov <eshmurodov.cooder@gmail.com>
2023-09-20 15:57:55 +0500

    added Roles classes

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,11 +13,20 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
+        $user = User::create([
             'name' => 'John',
             'email' => 'John123@mails.ru',
             'password' => Hash::make('123')
         ]);
+
+        $user2 = User::create([
+            'name' => 'Alisher',
+            'email' => 'eshmurodov.alisher@inbox.ru',
+            'password' => Hash::make('1111')
+        ]);
+
+        $user->roles()->attach([1, 3]);
+        $user2->roles()->attach(2);
 
 //        User::factory(10)->create();
 
